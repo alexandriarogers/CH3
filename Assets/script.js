7b738b93e21334cf5ac0d79ba631195dab826ebb
Alexandria Rogers <rogers@Alexandrias-MacBook-Pro.local>
2022-07-25 20:40:08 -0400

    second commit

--- Assets/script.js
+++ Assets/script.js
@@ -1,4 +1,4 @@
-// Assignment Code
+// Defining variables for all of the passwrd options and the generate button from the HTML
 var generateBtn = document.querySelector("#generate");
 var lowerCase = "abcdefghijklmnopqrstuvwxyz";
 var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -7,18 +7,24 @@ var symbols = "!!@#$%^&*()";
 
 
 
-// Write password to the #password input
+// The write password function
 function writePassword() {
  
-
-  var len = window.prompt("How many characters would you like your password to be? 8 to 128.");
+  // Defining variables for the input from user's choice
+  var len = window.prompt("How many characters would you like your password to be? Password length has to be 8 to 128 characters.");
+  //An if statment that lets the user if their password does not meet the lenth requirment 
+  if(len > 128 || len < 8){
+    window.alert("Your password has to be at least 8 characters and no more than 128.")
+    return;
+  }
   var addlower = window.prompt("Would you like lower case letters in your password? Y or N?");
   var addupper = window.prompt("Would you like upper case letters to ypur password? Y or N?");
   var addnums = window.prompt("Would you like numbers in your password? Y or N?")
   var addsymbols = window.prompt("Would you like symbols in your password? Y or N?")
+  // Defining an empty variable so that when the if statements run the contents of the varibles that defines the password parameters that the user wants used to create their password is inputed into this variable
   var parameters = "";
   
-
+  // If statements that add the contents of the varibles that define the password parameters that the user wants used to create their password
   if (addlower === "Y") {
     parameters = parameters + lowerCase;   
   }
@@ -31,13 +37,16 @@ function writePassword() {
   if (addsymbols === "Y"){
     parameters = parameters + symbols
   }
+  //Defining an empty varibale for the randomized content of the parameters variable
   var pass = "";
+  //Defining a length varibale to convert the input from the user for the length of their password from a string to an integer 
   var length = parseInt(len); 
+  // A for loop that that randomizes the contents of the parameters variable
   for (var i = 0; i < length; i++) {
     var rand = Math.floor(Math.random() * parameters.length);
     pass += parameters[rand];
   }
-
+  //Rentuns a window alert with the contents of the pass varible (which contains the randomized contents of the parameters variable)
   return window.alert("Your password is: " + pass);
   
 
---
+++ Readme.md
@@ -0,0 +1,8 @@
+This challenge is to create Password genorator.
+
+The Password generator asks the user what length they want their password to be and lets them know if that length does not meet the password length requirment of 8 to 128 characters.
+The Password generater asks if the user wants lower case, upper caser, numbers, or symbols added to their password.
+The password generator generates a random password with all the criteria that the user indicated they wanted added to their password.
+
+![Here is a image of what the challenge looks like.](./Assets/images/03-javascript-homework-demo.png)
+
